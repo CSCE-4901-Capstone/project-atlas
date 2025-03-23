16768d209bdc160e035f4291ed27b03bd2c2b4a9
Amaan-S <146875632+Amaan-S@users.noreply.github.com>
2025-03-22 20:38:54 -0500

    Updated Gemini_API with prompt engineering to suit role of a news gatherer.

--- server/api/services.py
+++ server/api/services.py
@@ -48,11 +48,40 @@ class FlightAPI(ExternalAPI):
             output.append(flight_dict)
         return output
 
-class GPT_API(ExternalAPI):
+class Gemini_API(ExternalAPI):
+    
 
     def EnterPrompt(prompt):
+        model = "google/gemini-2.0-pro-exp-02-05:free"
+        API_key = os.getenv("OPENROUTER_API_KEY")  #fetch the API_key from environment variables of the server
+        
+        headers = {
+        "Authorization": f"Bearer {API_key}",
+        "Content-Type": "application/json"
+        }
+        AI_Role = '''You are to assume the role of a global news gatherer.
+        Your main objective will be to identify the country being spoken of in the prompt,
+        and then provide information on the documentation needed to travel to that country assuming the 
+        user is an American national.'''
+        SendMessage = {
+            "model" = model,                #make sure the message is being sent to the gemini model
+            "messages":[
+                {#Message to define role of AI in prompt exchange
+                    "role": "system",   
+                    "type": "text",
+                    "text": AI_Role
+                },
+                {#Message to define the message being sent to the AI
+                    "role": "user",
+                    "type": "text",
+                    "text": prompt
+                }
+            ]
+            
+        }
         #pushing data to GPT to get processed data
         #additional prompt engineering will be added to ensure proper data fromat is expected
         
     def ReturnPrompt():
         #return data to be implemented from API call
+        pass
