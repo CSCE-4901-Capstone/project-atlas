a75e638238baf121246ecebd192087a5f5a6d072
Juan Correa <Juan.Correa@unt.edu>
2025-03-27 13:49:35 -0500

    Apply style changes according to #bug-report by @Daniel Podgorny. Filter checkboxes have not been edited and scrollbar has not been styled.

--- client/src/components/menu/AISummary.jsx
+++ client/src/components/menu/AISummary.jsx
@@ -10,14 +10,17 @@ const AISummary = ({choiceCountry}) =>{
     return(
         <>
             <ChoiceCountry choice={choiceCountry}/>
-            <p>
-                {/*Insert AI prompt return here and delete below filler text */}
-                International Relations and Politics Ukraine-Russia Ceasefire Negotiations: Ukrainian President Volodymyr Zelenskyy has called on U.S. President Donald Trump to 
-                leverage America's influence to press Russia into accepting a ceasefire in Ukraine. While U.S.-Russia talks have made some progress, significant challenges and 
-                mistrust persist. Russian President Vladimir Putin has suggested that Ukrainian troops in the Kursk region could surrender safely, but Zelenskyy remains skeptical 
-                of Russia’s intentions, accusing Putin of obstructing the peace process.
-                Global Leaders Advocate for Peace in Ukraine: Australian
-            </p>
+            <div className="text-container">
+                <p>
+                    {/*Insert AI prompt return here and delete below filler text */}
+                    International Relations and Politics Ukraine-Russia Ceasefire Negotiations: Ukrainian President Volodymyr Zelenskyy has called on U.S. President Donald Trump to 
+                    leverage America's influence to press Russia into accepting a ceasefire in Ukraine. While U.S.-Russia talks have made some progress, significant challenges and 
+                    mistrust persist. Russian President Vladimir Putin has suggested that Ukrainian troops in the Kursk region could surrender safely, but Zelenskyy remains skeptical 
+                    of Russia’s intentions, accusing Putin of obstructing the peace process.
+                    Global Leaders Advocate for Peace in Ukraine: Australian
+                </p>
+            </div>
+
         </>
     )
 }
