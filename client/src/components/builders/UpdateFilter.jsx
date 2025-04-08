d03cf08d55c1c4969c137d72c136a538b270e591
Jacob Gewin <jacobgewin@gmail.com>
2025-04-08 12:27:55 -0500

    Update Filter to add Weather functionality.

--- client/src/components/builders/UpdateFilter.jsx
+++ client/src/components/builders/UpdateFilter.jsx
@@ -1,9 +1,12 @@
 import Flights from 'src/components/builders/Flights';
+import Weather from 'src/components/builders/Weather';
 
 function UpdateFilter({ activeFilter }) {
   switch (activeFilter) {
       case 'Flights':
         return <Flights radius={2} />;
+      case 'Weather':
+        return <Weather radius={2} />;
       default:
         return null
   }
