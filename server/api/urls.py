40e7762ce4848b4742912268014b6c3ceeac92dd
Ivan Rivas <ivanrivas2004@gmail.com>
2025-04-09 09:06:39 -0500

    OpenWeatherMap API connection

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from api.views import FlightList
+from api.views import WeatherGridView
 
 urlpatterns = [
-        path('flights/', FlightList().as_view())
+        path('flights/', FlightList().as_view()),
+        path('weather-grid/', WeatherGridView().as_view()),
 ]
-
