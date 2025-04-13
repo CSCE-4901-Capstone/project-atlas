51664787c0f6d81d906c18a99830182f0ad9749e
Jacob Gewin <jacobgewin@gmail.com>
2025-04-12 23:08:11 -0500

    Changed wrapping method

--- client/src/components/builders/Weather.jsx
+++ client/src/components/builders/Weather.jsx
@@ -3,11 +3,10 @@ import { TextureLoader } from 'three';
 import { OrbitControls } from '@react-three/drei';
 
 function Weather() {
-  // palette '-98:4200ff;0:0061ff;15:00d5ff;21:a0ddff;23:ffffff;25:ffffcc;30:ffff00;40:ff8000;60:ff0000';
-  const apiKey = ''; // API key goes her and we're going to use .env in production!
-  const weatherUrl = `https://tile.openweathermap.org/map/temp_new/1/1/1.png?appid=${apiKey}&palette=-98:4200ff;0:0061ff;15:00d5ff;21:a0ddff;23:ffffff;25:ffffcc;30:ffff00;40:ff8000;60:ff0000`;
-
-  const weatherTexture = useLoader(TextureLoader, weatherUrl);
+  // Load the stitched global weather texture
+  const weatherTexture = useLoader(TextureLoader, '/stitched_weather.png');
+  
+  // Optional fallback if the main texture fails (e.g., local earth map)
   const fallbackTexture = useLoader(TextureLoader, '/images/earth2.jpg');
 
   return (
@@ -15,14 +14,14 @@ function Weather() {
       <ambientLight intensity={1} />
       <OrbitControls makeDefault />
       <mesh>
-        <sphereGeometry args={[2, 450, 450]} />
+        <sphereGeometry args={[2, 128, 128]} />
         <meshStandardMaterial
           map={weatherTexture || fallbackTexture}
         />
       </mesh>
     </>
   );
-  }
+}
 
 export default Weather;
 
