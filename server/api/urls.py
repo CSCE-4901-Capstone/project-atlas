59ae92649336db689c69a1db697056a84dcb6a6b
Jacob Gewin <jacobgewin@gmail.com>
2025-04-23 13:15:29 -0500

    Updated weather call (broken)

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from api.views import FlightList, CountryNews
 from api.views import WeatherGridView
 
+
 urlpatterns = [
         path('flights/', FlightList().as_view()),
-        path('weather/', WeatherGridView().as_view()),
+        path("weather/", WeatherGridView.as_view(), name="weather-grid"),
         path('AI/', CountryNews.as_view())
 ]
