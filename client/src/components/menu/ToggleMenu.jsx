51debe65676ecd8348786a768bc3e7d0a6440369
Juan Correa <juan.correa.swe@gmail.com>
2025-09-10 12:42:32 -0500

    ToggleMenu optimized for multiple filters. Custom radio buttons are styled in line with the rest of the menu. Need to organize filter items to reflect figma.

--- client/src/components/menu/ToggleMenu.jsx
+++ client/src/components/menu/ToggleMenu.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import '../../styles/Atlas.css'
 
-//Create array of prefilled filters then render based on the length of the filter
+//**Array is global! Consider the data being passed from the server instead!**
+
 
 // Have the toggle menu be built using an array of names
 function ToggleMenu({ onFilterSelection }) {
     const [selected,setSelected] = useState(null);
-
+    const filters = ["Flights", "Weather"];
 
   function handleFilterSelection(e) {
     const value = e.target.value;
@@ -20,30 +21,22 @@ function ToggleMenu({ onFilterSelection }) {
             <div className="title">
                 <h1>Filters</h1>
             </div>
-                <div id="toggle-radio-group">
-                    <label className="custom-label">
-                        <input
-                            type="checkbox"
-                            value="Weather"
-                            name='toggles'
-                            checked={selected === "Weather"}
-                            onChange={handleFilterSelection}
-                        />
-                        <span className="custom-radio"></span>Weather
+            <div id="toggle-radio-group">
+                {filters.map((filter) => (
+                    <label key={filter} id="custom-label">
+                    <input
+                        type="checkbox"
+                        value={filter}
+                        name="toggles"
+                        checked={selected === filter}
+                        onChange={handleFilterSelection}
+                    />
+                    <span className="custom-radio"></span>{filter}
                     </label>
-                    <label className="custom-label">
-                        <input
-                            type="checkbox"
-                            value="Flights"
-                            name='toggles'
-                            checked={selected === "Flights"}
-                            onChange={handleFilterSelection}
-                        />
-                        <span className="custom-radio"></span>Flights
-                    </label>
-                </div>
-        </div>
+                ))}
+            </div>
 
+        </div>
     )
 }
 
