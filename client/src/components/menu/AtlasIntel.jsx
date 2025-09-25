5cb37da7e0a8b371e1b74f6c079e843f64ce7397
Juan Correa <juan.correa.swe@gmail.com>
2025-09-25 11:00:27 -0500

    Prep AI summary for implementation

--- client/src/components/menu/AtlasIntel.jsx
+++ client/src/components/menu/AtlasIntel.jsx
@@ -17,12 +17,18 @@ function AtlasIntel({choiceMade}){
     },[])
 
     return(
-        <div id="atlas-intel">
+        <div 
+        id="atlas-intel"
+        className={choiceMade ? "active" : ""}
+        >
             <div className="atlas-title">
                 <h1>AI</h1>
             </div>
             <div className="atlas-response-container">
-                <p>{/*This is where the AI response would be received */}</p>
+                <p>
+                    {/*This is where the AI response would be received */}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
             </div>
         </div>
     )
