a787d440a99bc6fbb4e849659aa7aeb2c368be7c
Juan Correa <juan.correa.swe@gmail.com>
2025-09-25 11:45:39 -0500

    Set fade out and fade in animation on change of choice country.

--- client/src/components/menu/ChoiceCountry.jsx
+++ client/src/components/menu/ChoiceCountry.jsx
@@ -1,9 +1,32 @@
 import { useState, useEffect } from "react";
 
 const ChoiceCountry = ({choice}) => {
+    const [displayed,setDisplayed] = useState(choice)
+    console.log("Displayed: " + displayed)
+    /*Works but the effect is delayed and looks off */
+    const [fade, setFade] = useState(false);
+    useEffect(()=>{
+        if(choice == displayed){
+            return;
+        }
+        setFade(true);
+
+        const timeout = setTimeout(()=>{
+            setDisplayed(choice);
+            setFade(false);
+        }, 600);
+        return ()=>clearTimeout(timeout);
+    }, [choice, displayed])
+
     return(
-        <div className="country-choice">
-            <h1>{choice == null ? 'Earth' : choice}</h1>
+        <div 
+        className="country-choice"
+        >
+            <h1
+            className={fade ? "hide" : ""}
+            >
+                {displayed == null ? 'Earth' : displayed}
+            </h1>
         </div>
     )
 }
