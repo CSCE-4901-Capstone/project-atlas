a59244186d1d1725884f03372b94219211278a0e
Jacob Gewin <jacobgewin@gmail.com>
2025-10-19 13:23:14 -0500

    Added 30 second API call wait

--- server/api/services.py
+++ server/api/services.py
@@ -199,6 +199,7 @@ class WeatherAPIAsync:
         return row, col
 
     async def _fetch_weather(self, session, lat, lon):
+        await asyncio.sleep(30)
         url = "https://api.openweathermap.org/data/2.5/weather"
         params = {
             "lat": lat,
