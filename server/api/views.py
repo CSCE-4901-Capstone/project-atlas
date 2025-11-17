a0cf9677fee54cc30fb80a045010c66262ddd23c
Amaan-S <ajsiddiqui2468@gmail.com>
2025-11-16 21:09:36 -0600

    temporary changes for correcting NewsCongestion Filter

--- server/api/views.py
+++ server/api/views.py
@@ -12,6 +12,10 @@ from api.models import FlightModel
 from api.serializers import FlightSerializer
 from api.services import FlightAPI,NEWS_API,Agentic_AI, DisasterAPI, DB_Manager_NEWS    #,Gemini_API,
 
+
+from api.variables import data_1, data_2
+
+
 class FlightList(APIView):
     """
     Handles the connection to the external flight API
@@ -122,7 +126,9 @@ class Heatmap(APIView):
     
     
     def get(self,request,format=None):             #GET request for all the articles needed to populate congestion
-        # --- start background refresh thread  ---
+        Mass_Articles = []
+        
+        '''# --- start background refresh thread  ---
         if not hasattr(self, "refresh_thread") or not self.refresh_thread.is_alive():
             self.refresh_thread = threading.Thread(target=self.Start_DB_refresh, daemon=True)
             self.refresh_thread.start()
@@ -137,92 +143,14 @@ class Heatmap(APIView):
             print(f"Error encountered during DB_Pull: {e}")
             Mass_Articles = None        #set to none to execute default code
         
-        if Mass_Articles is None:       #default to following pre-defined json data if data doesn't load
+        if Mass_Articles is None:    '''   #default to following pre-defined json data if data doesn't load
             
-            Mass_Articles[0] = [
-  {
-    "city": "Mumbai",
-    "url": "https://economictimes.indiatimes.com/news/india/shah-banos-daughter-sends-legal-notice-to-emraan-hashmi-yami-gautams-haq/articleshow/125047347.cms",
-    "title": "Shah Bano's daughter sends legal notice to Emraan Hashmi, Yami Gautam's 'HAQ'",
-    "country": "India",
-    "latitude": 19.0760,
-    "longitude": 72.8777
-  },
-  {
-    "city": "Mumbai",
-    "url": "https://economictimes.indiatimes.com/news/new-updates/indians-went-crazy-after-south-africa-captain-laura-wolvaardt-thanks-team-india-on-twitter-it-turned-out-to-be-fake/articleshow/125047331.cms",
-    "title": "Indians went crazy after South Africa Captain Laura Wolvaardt thanks Team India on Twitter, it turned out to be fake",
-    "country": "India",
-    "latitude": 19.0751,
-    "longitude": 72.8786
-  },
-  {
-    "city": "Mumbai",
-    "url": "https://m.economictimes.com/markets/stocks/news/a-measured-move-opec-balances-growth-with-caution/opec-balances-expectations-and-caution/slideshow/125047485.cms",
-    "title": "A Measured Move: OPEC+ balances growth with caution",
-    "country": "India",
-    "latitude": 19.0769,
-    "longitude": 72.8768
-  },
-  {
-    "city": "Mumbai",
-    "url": "https://economictimes.indiatimes.com/tech/technology/pine-labs-ipo-could-unlock-esops-worth-rs-1360-crore/articleshow/125047189.cms",
-    "title": "Pine Labs IPO could unlock Esops worth Rs 1,360 crore",
-    "country": "India",
-    "latitude": 19.0755,
-    "longitude": 72.8789
-  },
-  {
-    "city": "Gurgaon",
-    "url": "https://indianexpress.com/article/cities/delhi/breakthrough-gurgaon-police-arrest-fugitive-gangster-deepak-nandal-rohtak-10342837/",
-    "title": "In another breakthrough, Gurgaon police arrest key associate of fugitive gangster Deepak Nandal in Rohtak",
-    "country": "India",
-    "latitude": 28.4595,
-    "longitude": 77.0266
-  },
-  {
-    "city": "Mumbai",
-    "url": "https://www.indiatimes.com/trending/haq-real-story-shah-bano-fight-for-justice-after-husband-abandoned-her-yami-gautam-emraan-hashmi/articleshow/125046584.html",
-    "title": "Haq real story: Shah Bano's historic fight for justice after her husband abandoned her",
-    "country": "India",
-    "latitude": 19.0775,
-    "longitude": 72.8752
-  },
-  {
-    "city": "Chennai",
-    "url": "https://www.thehindubusinessline.com/markets/stock-markets/markets-open-flat-amid-mixed-global-cues-shriram-finance-leads-gainers-with-5-rally/article70234957.ece",
-    "title": "Markets open flat amid mixed global cues; Shriram Finance leads gainers with 5% rally",
-    "country": "India",
-    "latitude": 13.0827,
-    "longitude": 80.2707
-  },
-  {
-    "city": "Mumbai",
-    "url": "https://www.rediff.com/movies/report/revealed-when-juhi-met-shah-rukh-for-the-first-time/20251103.htm",
-    "title": "REVEALED! When Juhi Met SRK For 1st Time!",
-    "country": "India",
-    "latitude": 19.0746,
-    "longitude": 72.8793
-  },
-  {
-    "city": "Delhi",
-    "url": "https://economictimes.indiatimes.com/news/science/pm-modi-launches-rs-1-lakh-crore-rdi-fund-to-spur-private-investment-in-research-development/articleshow/125047079.cms",
-    "title": "PM Modi launches Rs 1 lakh crore RDI Fund to spur private investment in research & development",
-    "country": "India",
-    "latitude": 28.6139,
-    "longitude": 77.2090
-  },
-  {
-    "city": "Melbourne",
-    "url": "https://berwicknews.starcommunity.com.au/sport/2025-11-03/cricket-world-rallies-together/",
-    "title": "Cricket world rallies together",
-    "country": "Australia",
-    "latitude": -37.8136,
-    "longitude": 144.9631
-  }
-]
-        else:
-            print("Data from firebase collections retrieved successfully!")
+        Mass_Articles[0] = data_1
+        
+        Mass_Articles[1] = data_2
+            
+        '''else:
+            print("Data from firebase collections retrieved successfully!")'''
 
         #This section of code calls for new articles to be pulled and updated to database (Not Returned to Frontend)
         
